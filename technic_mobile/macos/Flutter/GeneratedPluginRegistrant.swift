9d8cd2f81dc15f6b0d14ca5494e36195e2fcae02
Charles Cresci <crescicharles@gmail.com>
2025-12-21 19:46:35 -0600

    Mobile app successfully running in Android simulator

--- technic_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ technic_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,12 +8,9 @@ import Foundation
 import app_links
 import firebase_core
 import firebase_messaging
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import local_auth_darwin
-import package_info_plus
 import path_provider_foundation
-import sentry_flutter
 import shared_preferences_foundation
 import sign_in_with_apple
 import sqflite_darwin
@@ -22,12 +19,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
